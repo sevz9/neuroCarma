2838cbfea415f3b05a4047ff567a0c1b08a8ac6c
Abusagit <mirotvorez00@gmail.com>
2024-09-13 15:15:21 +0300

    add previous state handling

--- nirvana_utils.py
+++ nirvana_utils.py
@@ -29,7 +29,9 @@ def copy_out_to_snapshot(out, dump=True):
         print(f"Copy {out} to the snapshot path: {snapshot_path}")
 
         # Delete previous state to avoid memory explosion
-        os.system(f"rm {snapshot_path}/state")
+        
+        if os.path.exists(f"{snapshot_path}/state"):
+            os.system(f"rm {snapshot_path}/state")
         copy_tree(out, snapshot_path)
 
         if dump:
